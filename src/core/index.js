6ac2ee1d8d1a9a2f15acaf16a1e2a6b763c0ef28
ranckprogram <1474574022@qq.com>
2019-06-13 00:02:40 +0800

    开发打印日志函数+ 多文件处理

--- src/core/index.js
+++ src/core/index.js
@@ -1,16 +1,25 @@
-const fs = require('fs')
+const fs = require("fs");
+const path = require("path");
 
-function updateFileMatchPlace(file, params, callback) {
-    const fileStr = fs.readFileSync(file, 'utf8')
-
-    // 多个字符创匹配
-    const regStr = params.map(item => item.key).join('|');
-    const reg = new RegExp(`(${regStr})`, 'g')
-    const newFileStr = fileStr.replace(reg, function (a, b, c) {
-        const param = params.filter(item => item.key === a)[0]
-        return `${a}\n${param.value}`
+function updateFileMatchPlace(file, params) {
+    return new Promise(function (resolve, reject) {
+        try {
+            const fileStr = fs.readFileSync(file, 'utf8')
+            // 多个字符创匹配
+            const regStr = params.map(item => item.key).join('|');
+            const reg = new RegExp(`(${regStr})`, 'g')
+            const newFileStr = fileStr.replace(reg, function (a, b, c) {
+                const param = params.filter(item => item.key === a)[0]
+                return `${a}\n${param.value}`
+            })
+            const newFileName = file.substr(0, file.length-4);
+            fs.writeFileSync(path.join(`${newFileName}-change.txt`), newFileStr)
+            resolve()
+        } catch (e) {
+            reject(e)
+        }
     })
-    fs.writeFileSync(`${file}-change.txt`, newFileStr)
+
 }
 
 exports.updateFileMatchPlace = updateFileMatchPlace
